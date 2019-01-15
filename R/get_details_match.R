2f43d7540622d84153d8cc3a769a19962c6e8816
Andryas <Andryaas@gmail.com>
2019-01-15 20:08:31 -0200

    mod a forma de coleta. Primeiro partidas, depois jogadores

---
+++ R/get_details_match.R
@@ -0,0 +1,164 @@
+#!/usr/bin/env Rscript
+
+# ------------------------------------------------------------------------------
+# Libraries
+# ------------------------------------------------------------------------------
+library(RDota2)
+library(tcltk)
+library(mongolite)
+library(jsonlite)
+
+# ------------------------------------------------------------------------------
+# Configs
+# ------------------------------------------------------------------------------
+# 1: keyapi & 2: .RData of ID's is required
+args <- commandArgs(TRUE)
+
+# args <- c(readRDS("~/DotA2/data/keyapi.RData")[1],
+#           "id-2019-01-04-.RData")
+
+# register key
+key_actions(action = "register_key", value = args[1])
+
+setwd("~/DotA2/data/id")
+
+id <- readRDS(args[2])
+id <- unlist(id)
+N <- length(id)
+
+# ------------------------------------------------------------------------------
+# MongoDB
+# ------------------------------------------------------------------------------
+m <- mongo(collection = "match", db = "dota2")   # Match
+
+# ------------------------------------------------------------------------------
+# Clean ID's
+# ------------------------------------------------------------------------------
+# duration <= 900 & players != 10 & no information (BAD ID's)
+# lobby_type not in (0, 2, 5, 7) & game_mode not in (0, 1, 2, 14, 22)
+if (length(
+    list.files(
+        pattern = paste0(gsub(".RData|.+/", "", args[2]), "bad_id"))) >= 1) {
+    badid <- readRDS(paste0(gsub(".RData|.+/", "", args[2]), "bad_id.RData"))
+} else {
+    badid <- data.frame(stringsAsFactors = FALSE)
+}
+
+# Storaged IDs in MongoDB
+si <- m$find(fields = '{"_id": true}')[[1]]
+id <- id[!(id %in% si)]
+
+# Remove bad IDs
+if (nrow(badid) > 0) {
+    id <- id[!(id %in% badid$match_id)]
+}
+
+# ------------------------------------------------------------------------------
+# Collect Details Match
+# ------------------------------------------------------------------------------
+
+# tcltk
+pb <- tkProgressBar(title = args[2], min = 0, max = N, width = 300)
+
+while (length(id) > 0) {
+    # cat(paste0(N - length(id), "/", N), sep = "\n")
+    setTkProgressBar(pb, N - length(id),
+                     label = paste0(round((N - length(id))/N * 100, 3), " %"))
+    content <-  try(
+        R.utils::withTimeout(
+                     get_match_details(match_id = id[1]),
+                     timeout = 10,
+                     onTimeout = "silent"
+                 ),
+        silent = TRUE
+    )
+
+    if ("content" %in% names(content)) {
+        if ("error" %in% names(content[["content"]])) {
+            badid <- rbind(badid, data.frame(match_id = id[1], type = "error"))
+            saveRDS(badid, paste0(gsub(".RData", "", args[2]), "bad_id.RData"))
+            id <- id[-1]
+            next
+        }
+    }
+
+    # --------------------------------------------------------------------------
+    # https://partner.steamgames.com/doc/webapi_overview/responses
+    # 200: success
+    # 400: Bad request
+    # 401: Unauthorized
+    # 403: Forbidden
+    # 404: Not found
+    # 429: Too many requests
+    # 500: Internet server error
+    # 503: Service unvavailable
+
+    if (content$response[["status_code"]] %in% c(401, 403)) {
+        cat(paste0("Access is denied. Retrying will not help.",
+                   "Please verify your key= parameter.\n\n", args[1]),
+            file = paste0("log", args[2], ".log"))
+        # file.remove(args[2])
+        stop(paste0("Access is denied. Retrying will not help.",
+                    "Please verify your key= parameter."))
+    }
+
+    if (content$response[["status_code"]] %in% c(503, 429, 500)) {
+        Sys.sleep(20)
+        next
+    }
+
+    if (content$response[["status_code"]] %in% c(400, 404)) {
+        badid <- rbind(badid, data.frame(match_id = id[1],
+                                         type = "bad_request"))
+        saveRDS(badid, paste0(gsub(".RData", "", args[2]), "bad_id.RData"))
+        id <- id[-1]
+        next
+    }
+    # --------------------------------------------------------------------------
+
+    ## Lobby_type & game_mode rules
+    if (!(content$content$lobby_type %in% c(0, 2, 5, 7)) |
+        !(content$content$game_mode %in%  c(0, 1, 2, 14, 22))) {
+        badid <- rbind(badid,
+                       data.frame(match_id = id[1], type = "lobbygame"))
+        saveRDS(badid, paste0(gsub(".RData", "", args[2]),
+                              "bad_id.RData"))
+        id <- id[-1]
+        next
+    }
+
+    # ## All account_id public
+    # account_id <- sapply(content$content$players, function(x) x$account_id)
+    # if (sum(account_id %in% c(4294967295)) > 1) {
+    #     badid <- rbind(badid, data.frame(match_id = id[1],
+    #                                      type = "players"))
+    #     saveRDS(badid, paste0(gsub(".RData", "", args[2]),
+    #                           "bad_id.RData"))
+    #     id <- id[-1]
+    #     next
+    # }
+
+    ## If the match lasted less than 900 seconds DELETE
+    if (content$content$duration <= 900) {
+        badid <- rbind(badid,
+                       data.frame(match_id = id[1], type = "duration"))
+        saveRDS(badid, paste0(gsub(".RData", "", args[2]),
+                              "bad_id.RData"))
+        id <- id[-1]
+        next
+    }
+
+
+    # Add match in database
+    # _pi: player information: 0 FALSE 1 TRUE
+    m$insert(toJSON(append(
+          list("_id" = content$content$match_id,
+               "_pi" = 0), content$content
+      ), auto_unbox = TRUE),
+      stop_on_error = TRUE)
+
+    id <- id[-1]
+}
+close(pb)
+file.remove(args[2])
+# file.remove(paste0(gsub(".RData", "", args[2]), "bad_id.RData"))
---
+++ R/get_details_player.R
@@ -0,0 +1,263 @@
+#!/usr/bin/env Rscript
+
+# ------------------------------------------------------------------------------
+# Libraries
+# ------------------------------------------------------------------------------
+library(RDota2)
+library(mongolite)
+library(jsonlite)
+library(lubridate)
+library(dplyr)
+library(tcltk2)
+
+# 1: keyapi 2: match_id
+args <- commandArgs(TRUE)
+# args <- c(readRDS("~/DotA2/data/keyapi.RData")[7], "J7.RData")
+
+# register key
+key_actions(action = "register_key", value = args[1])
+
+# ------------------------------------------------------------------------------
+# MongoDB
+# ------------------------------------------------------------------------------
+m <- mongo(collection = "match", db = "dota2")   # Match
+m2 <- mongo(collection = "player", db = "dota2") # Player
+
+setwd("~/DotA2/data/id/")
+
+# IDs to collect
+ids <- readRDS(args[2])
+
+#! Limpa listas nulas
+index <- which(sapply(ids, nrow) == 0)
+ids <- ids[-index]
+N <- length(ids)
+
+ids_collected <- m$find(query = '{"_pi": 1}',
+                        fields = '{"_id": true}')$`_id`
+
+index2 <- sapply(ids, function(x) unique(x$match_id)) %in% ids_collected
+
+if (length(which(index2)) > 0) {
+    i <- max(which(index2)) + 1 ## i
+} else {
+    i <- 1
+}
+
+# ------------------------------------------------------------------------------
+# Collect Details Player
+# ------------------------------------------------------------------------------
+root <- tktoplevel()
+tktitle(root) <- args[2]
+
+l1 <- tk2label(root, args[2])
+pb1 <- tk2progress(root, length = 300)
+tkconfigure(pb1, value=0, maximum = length(ids))
+
+l2 <- tk2label(root, "Collecting Player's Details")
+pb2 <- tk2progress(root, length = 300)
+
+tkpack(l1)
+tkpack(pb1)
+tkpack(l2)
+tkpack(pb2)
+
+for (i in i:N) {
+    tkconfigure(l1, text = paste0(i, "/", N))
+    tkconfigure(pb1, value = i)
+
+    p <- ids[[i]]
+
+    if (nrow(p) == 0) {
+        next
+    }
+
+    for (j in 1:nrow(p)) {
+
+        #! Historico anterior do jogador anterior a partida coletada
+        history <-  try(
+            R.utils::withTimeout(
+                         get_match_history(account_id = p$account_id[j],
+                                           start_at_match_id = p$match_id[j]),
+                         timeout = 10,
+                         onTimeout = "silent"
+                     ),
+            silent = TRUE
+        )
+
+        player_match_id <- sapply(history$content$matches, function(x) {
+            x$match_id
+        })
+
+        #! Informação armazenada do jogador no MongoDB
+        player_db <-  m2$find(
+                             query = paste0('{"_id": ', p$account_id[j], '}'),
+                             fields = paste0('{"details.match_id": true }')
+                         )
+
+        player_badid_db <- m2$find(
+                                  query = paste0('{"_id": ', p$account_id[j], '}'),
+                                  fields = paste0('{"badid": true }')
+                              )
+
+        #! Remove partidas já coletadas
+        if (nrow(player_db) > 0) {
+            player_match_id <- player_match_id[!(player_match_id %in%
+                                                 player_db$details[[1]]$match_id)]
+        }
+
+        #! Remove partidas consideradas badid
+        if (nrow(player_badid_db) > 0) {
+            player_match_id <-
+                player_match_id[!(player_match_id %in%
+                                  player_badid_db$badid[[1]])]
+        }
+
+        ## Se não houver partidas historicas do jogador j
+        if (length(player_match_id) == 0) {
+            tkconfigure(pb2, value=0, maximum = 10)
+            tkconfigure(l2, text = paste0("Jogador ", j, " de ", nrow(p)))
+            tkconfigure(pb2, value = 10)
+            next
+        }
+
+        tkconfigure(pb2, value=0, maximum = length(player_match_id))
+
+        #! Coleta partidas anteriores do jogador j
+        w <- 0
+        while (length(player_match_id) > 0) {
+            w <- w + 1
+            tkconfigure(l2, text = paste0("Jogador ", j, " de ", nrow(p)))
+            tkconfigure(pb2, value = w)
+
+            content <-  try(
+                R.utils::withTimeout(
+                             get_match_details(match_id = player_match_id[1]),
+                             timeout = 10,
+                             onTimeout = "silent"
+                         ),
+                silent = TRUE
+            )
+
+
+            if ("content" %in% names(content)) {
+                if ("error" %in% names(content[["content"]])) {
+                    next
+                }
+            }
+
+            # --------------------------------------------------------------------------
+            # https://partner.steamgames.com/doc/webapi_overview/responses
+            # 200: success
+            # 400: Bad request
+            # 401: Unauthorized
+            # 403: Forbidden
+            # 404: Not found
+            # 429: Too many requests
+            # 500: Internet server error
+            # 503: Service unvavailable
+
+            if (content$response[["status_code"]] %in% c(401, 403)) {
+                cat(paste0("Access is denied. Retrying will not help.",
+                           "Please verify your key= parameter.\n\n", args[1]),
+                    file = paste0("log", args[2], ".log"))
+                # file.remove(args[2])
+                stop(paste0("Access is denied. Retrying will not help.",
+                            "Please verify your key= parameter."))
+            }
+
+            if (content$response[["status_code"]] %in% c(503, 429, 500)) {
+                Sys.sleep(60)
+                next
+            }
+
+
+            if (content$response[["status_code"]] %in% c(400, 404)) {
+                player_match_id <- player_match_id[-1]
+                next
+            }
+
+            # --------------------------------------------------------------------------
+            ## Lobby_type & game_mode rules
+            if (!(content$content$lobby_type %in% c(0, 2, 5, 7)) |
+                !(content$content$game_mode %in%  c(0, 1, 2, 14, 22))) {
+                m2$update(
+                       query = paste0('{"_id":', p$account_id[j], '}'),
+                       update =  toJSON(
+                           list("$addToSet" = list(badid = content$content$match_id)),
+                           auto_unbox = TRUE
+                       ),
+                       upsert = TRUE
+                   )
+                player_match_id <- player_match_id[-1]
+                next
+            }
+
+            ## If the match lasted less than 900 seconds DELETE
+            if (content$content$duration <= 900) {
+                m2$update(
+                       query = paste0('{"_id":', p$account_id[j], '}'),
+                       update =  toJSON(
+                           list("$addToSet" = list(badid = content$content$match_id)),
+                           auto_unbox = TRUE
+                       ),
+                       upsert = TRUE
+                   )
+                player_match_id <- player_match_id[-1]
+                next
+            }
+
+            player <- content$content$players
+            player <- lapply(player, function(x) {
+                if ("ability_upgrades" %in% names(x)) {
+                    players_au <<- x$ability_upgrades
+                    cbind.data.frame(x[-which(names(x) == "ability_upgrades")])
+                } else {
+                    players_au <<- NULL
+                    cbind.data.frame(x)
+                }}) %>%
+                bind_rows() %>%
+                mutate(position = 1:10)
+
+            player <- player %>%
+                filter(account_id == p$account_id[j]) %>%
+                mutate(
+                    duration = content$content$duration,
+                    radiant_win = content$content$radiant_win,
+                    start_time = content$content$start_time,
+                    match_seq_num = content$content$match_seq_num,
+                    match_id = content$content$match_id
+                )
+
+            if (is.null(players_au)) {
+                player <- list("$addToSet" = list(details = as.list(player)))
+            } else {
+                player <-
+                    list("$addToSet" =
+                             list(
+                                 details = append(
+                                     as.list(player),
+                                     list(ability_upgrades =
+                  content$content$players[[player$position]]$ability_upgrades))))
+            }
+
+            #! Add Jogador
+            m2$update(
+                   query = paste0('{"_id":', p$account_id[j], '}'),
+                   update =  toJSON(player, auto_unbox = TRUE),
+                   upsert = TRUE
+               )
+
+            player_match_id <- player_match_id[-1]
+            tcl("update")
+        }
+    }
+
+
+    m$update(paste0('{"_id":', p$match_id[1], '}'),
+             '{"$set": {"_pi": 1}}')
+
+    tcl("update")
+}
+
+tkdestroy(root)
