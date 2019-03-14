f7058d2e27b20b6d84b115e32785360a99b4669b
Andryaas <Andryaas@gmail.com>
2019-03-14 07:26:28 -0300

    update packages e process

--- R/packages.R
+++ R/packages.R
@@ -1,3 +1,3 @@
 if (!("pacman" %in% installed.packages()[, "Package"])) install.packages("pacman")
 pacman::p_load(mongolite, stringr, dplyr, tidyr, parallel, plyr, jsonlite,
-               rvest, httr, RDota2, processx, R.utils, lubridate)
+               rvest, httr, RDota2, processx, R.utils, lubridate,  parallel)
--- R/process.R
+++ R/process.R
@@ -1,95 +1,109 @@
+source("packages.R")
 setwd("~/Dota2/data")
+options(dplyr.width = Inf)
 
-library(mongolite)
+## limit multipler 10
+process <- function(limit = 10000) {
+    m <- mongo(collection = "match", db = "dota2")
+    m2 <- mongo(collection = "player", db = "dota2")
 
-# Mongo
-m <- mongo(collection = "match", db = "dota2")
-m2 <- mongo(collection = "player", db = "dota2")
+    files <- list.files(pattern = "md_.+.rds")
+    if (length(files) == 0) match_id <- "[]" else match_id <- jsonlite::toJSON(do.call(c, lapply(files, readRDS)))
 
-# match_id <- if (exists("match_id.RData")) readRDS("match_id.RData") else 0
+    df <- m$find(paste0('{"_pi": 1, "players_available": 10, "match_id":{ "$nin":', match_id,' }}'), limit = limit)
 
-df <- m$find('{"_pi": 1}', limit = 10000)
+    df <- as_tibble(df)
 
-player_info <- df$players
+    player_info <- df$players
 
-player_info <- mapply(x = player_info, i = df$match_id, j = df$start_time, 
-                      function(x, i, j) {
-                          x$match_id <- i
-                          x$start_time <- j
-                          x$team <- rep(c("radiant","dire"), each = 5)
-                          x
-                      })
+    player_info <- mapply(x = player_info, i = df$match_id, j = df$start_time, 
+                          function(x, i, j) {
+                              x$match_id <- i
+                              x$start_time <- j
+                              x$team <- rep(c("radiant","dire"), each = 5)
+                              x
+                          }, SIMPLIFY = FALSE)
 
-player_info <- plyr::rbind.fill(player_info)
+    player_info <- plyr::rbind.fill(player_info)
 
-player_info <- player_info[, c("match_id", "account_id", "hero_id",
-                               "start_time", "team")]
+    player_info <- player_info[, c("match_id", "account_id", "hero_id",
+                                   "start_time", "team")] %>% as_tibble()
 
-# ==============================================================================
-cols <- c("account_id", "kills", "deaths", "assists", "last_hits", "denies",
-          "gold_per_min", "xp_per_min", "hero_damage", "tower_damage",
-          "hero_healing", "gold", "gold_spent", "scaled_hero_damage",
-          "scaled_tower_damage", "duration", "ability_upgrades_mean",
-          "ability_upgrades_var", "win_player"
-          )
+    cols <- c("account_id", "kills", "deaths", "assists", "last_hits", "denies",
+              "gold_per_min", "xp_per_min", "hero_damage", "tower_damage",
+              "hero_healing", "gold", "gold_spent", "scaled_hero_damage",
+              "scaled_tower_damage", "duration", "ability_upgrades_mean",
+              "ability_upgrades_var", "win_player"
+              )
 
-# sum(player_info$account_id == 4294967295) ## account_id privado
-teste <- player_info[1:10, ]
+    num_cl <- detectCores()                     ## get all cores
+    cl <- makeCluster(num_cl)           
+    player_info$k <- rep(1:4, each = limit/4)      ## split in four folds
+    ll <- split(player_info, player_info$k)
+    clusterExport(cl, list("m", "m2", "cols"), envir = environment())  ## export objetcs
+    # clusterExport(cl, list("m", "m2", "cols"))  ## export objetcs
+    clusterEvalQ(cl, library(mongolite))        ## call library
 
+    x <- parLapply(cl, ll, function(l) {
 
-# Últimas 10 partidas
-x <- mapply(SIMPLIFY = FALSE,
-       account_id = teste$account_id,
-       start_time = teste$start_time,
-       function(account_id, start_time) {
-           ## ADD
-           x <- m2$find(paste0('{"_id":', account_id, ', "details.start_time": {"$lt": ',
-                               start_time,'}}'))$details[[1]]
-           x <- x[order(x$start_time, decreasing = TRUE), ][1:(ifelse(nrow(x) < 10, nrow(x), 10)), ]
+        x <- mapply(SIMPLIFY = FALSE,
+               account_id = l$account_id,
+               start_time = l$start_time, 
+               function(account_id, start_time) {
+                   x <- m2$find(paste0('{"_id":', account_id, ', "details.start_time": {"$lt": ',
+                                       start_time,'}}'))$details[[1]]
+                   
+                   if (is.null(x) | !all(c(cols[-c(17:19)], "ability_upgrades") %in% names(x))) {
+                       NULL
+                   } else {
+                       x <- x[order(x$start_time, decreasing = TRUE), ][1:(ifelse(nrow(x) < 10, nrow(x), 10)), ]
+                       
+                       # cria variaveis summarisadas da variável ability_upgrades
+                       x$ability_upgrades_mean <- sapply(x$ability_upgrades,
+                                                         function(x) {
+                                                             if (is.null(x) | length(x) == 0 | nrow(x) == 0) NA
+                                                             else mean(c(x$time[1], diff(x$time)))
+                                                         })
 
-           # cria variaveis summarisadas da variável ability_upgrades
-           x$ability_upgrades_mean <- sapply(x$ability_upgrades,
-                                             function(x) {
-                                                 if (is.null(x)) NULL
-                                                 else mean(c(x$time[1], diff(x$time)))
-                                             })
+                       x$ability_upgrades_var <- sapply(x$ability_upgrades,
+                                                        function(x) {
+                                                            if (is.null(x) | length(x) == 0 | nrow(x) == 0) NA
+                                                            else var(c(x$time[1], diff(x$time)))
+                                                        })
 
-           x$ability_upgrades_var <- sapply(x$ability_upgrades,
-                                            function(x) {
-                                                if (is.null(x)) NULL
-                                                else var(c(x$time[1], diff(x$time)))
-                                            })
+                       # Calcula se o jogador venceu a partida
+                       x$win_player <- as.integer((isTRUE(x$radiant_win) & x$position <= 5) |
+                                                  (!isTRUE(x$radiant_win) & x$position > 5))
+                       
+                       x[, cols]
+                   }
+               })
+        
+    })
+    
+    stopCluster(cl)
+    
+    x <- lapply(x, bind_rows)
+    x <- as_tibble(bind_rows(x))
 
-           # Calcula se o jogador venceu a partida
-           x$win_player <- as.integer((isTRUE(x$radiant_win) & x$position <= 5) |
-                                      (!isTRUE(x$radiant_win) & x$position > 5))
-           
-           x[, cols]
-       })
+    x2 <- x %>%
+        group_by(account_id) %>%
+        summarise_all(.funs = funs(mean, var)) %>%
+        mutate(n =  as.integer(table(x$account_id))) %>%
+        left_join(player_info[, c("account_id", "hero_id", "team", "match_id")],
+                  by = "account_id") %>% 
+        as_tibble() %>%
+        arrange(match_id, team)
 
-x <- do.call(rbind, x)
+    uq_match_id <- unique(x2$match_id)
 
-# library(ggplot2)
-# names(x)
-# 
-# x$account_id <- as.factor(x$account_id)
-# x$win_player <- as.factor(x$win_player)
-# ggplot(x, aes(x = last_hits, y = gold_spent,
-#               group = win_player, color = win_player)) +
-#     geom_point() + facet_wrap(~account_id)
+    files <- list.files()
+    # files <- "data_1.rds"
+    files <- str_extract(files, "(?<=data_).+(?=.rds)")
 
-# Calcula estatísticas do jogador para cada atributo média e variância.
-x2 <- aggregate(x[, 2:ncol(x)], by = list(account_id = x$account_id),
-                FUN = function(x) c(MEAN = mean(x, na.rm = TRUE),
-                                    VAR = var(x, na.rm = TRUE)))
-x2$n <- as.integer(table(x$account_id))
-x2 <- merge(x2, teste[, c("account_id", "hero_id", "team")], by = "account_id")
-x2 <- x2[order(x2$team, decreasing = TRUE), ]
-
-
-# upate db.match com estatísticas de partidas passadas de cada jogador presente
-# na partida
-
-
-
-
+    i <- ifelse(length(files) == 0, 1, max(as.integer(files), na.rm = TRUE) + 1)
+    saveRDS(x2, paste0("data_", i, ".rds"))
+    saveRDS(uq_match_id, paste0("md_", i, ".rds"))
+    m$disconnect()
+    m2$disconnect()
+}
