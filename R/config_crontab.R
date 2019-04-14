9558b3a0ecd77a35af46874dc0f031567b7303ea
Andryaas <Andryaas@gmail.com>
2019-04-14 10:10:07 -0300

    add config_crontab, description and namespace

---
+++ R/config_crontab.R
@@ -0,0 +1,25 @@
+#' @title Schedule R scripts/processes with the cron scheduler.
+#'
+#' @description This function config the scripts to collect Dota2 data.
+#' collect_id: everyday 00:10
+#' collect_match_details: everyday 00:15
+#' collect_playres_details: everyday 02:20
+#'
+#'
+#' @export
+
+config_cronR <- function() {
+    if (length(setdiff("cronR", rownames(installed.packages()))) > 0) {
+        install.packages(setdiff("cronR", rownames(installed.packages())))  
+    }
+
+    cmd_collect_id <- "Rscript -e \"RDota2Plus::collect('collect_id')\" &"
+    cmd_collect_match_details <- "Rscript -e \"RDota2Plus::collect('collect_match_details')\" &"
+    cmd_collect_players_details <- "Rscript -e \"RDota2Plus::collect('collect_players_details')\" &"
+
+    cronR::cron_add(cmd_collect_id, id = "collect_id", at = "00:10")
+    cronR::cron_add(cmd_collect_id, id = "collect_match_details", at = "00:15")
+    cronR::cron_add(cmd_collect_id, id = "collect_players_details", at = "02:20")
+    cat("\t\tDone\t\t")
+}
+
